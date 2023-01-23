a1afed2e8596894853c0b5cc97f52670c7183fdc
Aditya Raj <54466050+justabeginner10@users.noreply.github.com>
2023-01-23 23:57:09 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
